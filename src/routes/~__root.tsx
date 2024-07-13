00cfcf599bae3ae6754a7f54c420ff99e98b391b
andrey.khludenev <andrey.khludenev@fivetran.com>
2024-07-14 00:17:17 +0200

    Add nav menu

--- src/routes/~__root.tsx
+++ src/routes/~__root.tsx
@@ -1,18 +1,39 @@
+import { Settings as SettingsIcon } from 'lucide-react';
+import {
+  NavigationMenu,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
-import './~__root.css';
+import { Button } from '@/components/ui/button';
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/settings'>Settings</Link>
-        <Link to='/repositories/$repositoryId/codeowners' params={{ repositoryId: 'any' }}>
-          Repository codeowners
-        </Link>
+      <div className='flex justify-between'>
+        <NavigationMenu className='mb-6'>
+          <NavigationMenuList>
+            <Link to='/settings'>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Settings
+              </NavigationMenuLink>
+            </Link>
+
+            <Link to='/repositories/$repositoryId/codeowners' params={{ repositoryId: 'any' }}>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Repository codeowners
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuList>
+        </NavigationMenu>
+        <div className='flex gap-2'>
+          <Button variant='outline' size='icon'>
+            <SettingsIcon className='h-5 w-5' />
+          </Button>
+        </div>
       </div>
-      <hr />
       <Outlet />
       <TanStackRouterDevtools />
     </>
