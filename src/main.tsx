1b458ae74d7eeb9ff94ad1418049c84faa5ce217
andrey.khludenev <andrey.khludenev@fivetran.com>
2024-07-14 12:24:32 +0200

    Add dropdown and theme toggle

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './main.css';
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from './components/theme/theme-provider';
 
 // Create a new router instance
 const router = createRouter({ routeTree });
@@ -25,7 +26,9 @@ if (!rootElement.innerHTML) {
   root.render(
     <React.StrictMode>
       <QueryClientProvider client={QUERY_CLIENT}>
-        <RouterProvider router={router} />
+        <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+          <RouterProvider router={router} />
+        </ThemeProvider>
       </QueryClientProvider>
     </React.StrictMode>,
   );
--- src/routes/~__root.tsx
+++ src/routes/~__root.tsx
@@ -8,6 +8,7 @@ import {
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import { Button } from '@/components/ui/button';
+import { ThemeToggle } from '@/components/theme/ThemeToggle';
 
 export const Route = createRootRoute({
   component: () => (
@@ -15,12 +16,6 @@ export const Route = createRootRoute({
       <div className='flex justify-between'>
         <NavigationMenu className='mb-6'>
           <NavigationMenuList>
-            <Link to='/settings'>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Settings
-              </NavigationMenuLink>
-            </Link>
-
             <Link to='/repositories/$repositoryId/codeowners' params={{ repositoryId: 'any' }}>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                 Repository codeowners
@@ -29,9 +24,12 @@ export const Route = createRootRoute({
           </NavigationMenuList>
         </NavigationMenu>
         <div className='flex gap-2'>
-          <Button variant='outline' size='icon'>
-            <SettingsIcon className='h-5 w-5' />
-          </Button>
+          <Link to='/settings'>
+            <Button variant='outline' size='icon'>
+              <SettingsIcon className='h-5 w-5' />
+            </Button>
+          </Link>
+          <ThemeToggle />
         </div>
       </div>
       <Outlet />
